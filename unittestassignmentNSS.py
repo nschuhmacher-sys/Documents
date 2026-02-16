c8718117a4346bc4d180fca57a81918d45534cc8
Nicholas Schuhmacher <nschuhmacher@ivytech.edu>
2026-02-16 15:58:09 -0500

    Modified templates to display posts from database.

---
+++ unittestassignmentNSS.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == "__main_":
+    unittest.main()
+
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+def test_sum_tuple():
+    assert sum((1, 2, 2)) == 6, "Should be 6"
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
+
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == "__main_":
+    unittest.main()
+
